c91b4c53087ae6fb7ee9808c5fe6234cb3074548
Graeme Brown <graemehbrown@googlemail.com>
2018-01-09 18:39:44 +0000

    Added Book class.
    Added constructor to Library class which initialises an empty list for the collection of books.
    
    Added stockCount and addBook methods to Library.

--- main/java/Library.java
+++ main/java/Library.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> collection;
+
+    public Library(){
+        this.collection = new ArrayList<Book>();
+    }
+
+    public int stockCount() {
+        return this.collection.size();
+    }
+
+    public void addBook(Book book) {
+        this.collection.add(book);
+    }
 }
--- test/java/LibraryTest.java
+++ test/java/LibraryTest.java
@@ -1,11 +1,31 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
 
-    Library library;
+    Library library, library2;
+    Book book;
 
     @Before
     public void before(){
         library = new Library();
+        library2 = new Library();
+        book = new Book();
+        library2.addBook(book);
+
+    }
+
+    @Test
+    public void canCountLibraryStock(){
+        assertEquals(1, library2.stockCount());
+    }
+
+    @Test
+    public void addBookToLibraryStockIncreasesStockCount(){
+        int stockCountBeforeAdd = library.stockCount();
+        library.addBook(book);
+        assertEquals(stockCountBeforeAdd+1, library.stockCount());
     }
 }
